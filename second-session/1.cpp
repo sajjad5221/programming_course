ec6e554098b8856e9f3b10f3051d06fc008a7cb1
Sajjad Momeni <sajjad5221@gmail.com>
2022-11-29 20:22:12 +0330

    chore: add second session folder

---
+++ second-session/1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    switch(n){
+        case 1:
+            cout << "hello";
+            break;
+        case 2:
+            cout << "bye";
+            break;
+        default:
+            cout << "goodbye";
+    }
+    
+    return 0;
+}
+
